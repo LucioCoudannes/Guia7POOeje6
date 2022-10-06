91baef0f32a336f621ea4356237a80170cbb776c
Lucio Coudannes <luciocoudannes@gmail.com>
2022-10-05 21:45:12 -0300

    commit1

---
+++ src/guia7pooeje6/Guia7POOEje6.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guia7pooeje6;
+
+/**
+ *
+ * @author A288808
+ */
+public class Guia7POOEje6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
